2cd0e8c1b0e7ef4edbd0e87767cc3538e5ec594d
qdkhan <qdkhan05@gmail.com>
2024-06-25 08:43:25 +0530

    Supplier create Page Modified

--- resources/views/supplier/create.blade.php
+++ resources/views/supplier/create.blade.php
@@ -7,9 +7,10 @@
         <nav>
             <ol class="breadcrumb">
                 <li class="breadcrumb-item"><a href="index.html">Home</a></li>
-                <li class="breadcrumb-item">Forms</li>
-                <li class="breadcrumb-item active">Layouts</li>
+                <li class="breadcrumb-item">Supplier</li>
+                <li class="breadcrumb-item active">Supplier</li>
             </ol>
+            <a href="{{route('supplier.index')}}" class="btn btn-primary btn-sm"> Show List </a>
         </nav>
     </div>
 
@@ -19,25 +20,41 @@
             <div class="col-lg-12">
                 <div class="card">
                     <div class="card-body">
-                        <h5 class="card-title">Vertical Form</h5>
+                        <h5 class="card-title">Supplier Add</h5>
 
                         <!-- Vertical Form -->
                         <form class="row g-3">
                             <div class="col-6">
-                                <label for="inputNanme4" class="form-label">Your Name</label>
-                                <input type="text" class="form-control" id="inputNanme4">
+                                <label for="name" class="form-label">Supplier name</label>
+                                <input type="text" class="form-control" id="name" name="name">
                             </div>
                             <div class="col-6">
-                                <label for="inputEmail4" class="form-label">Email</label>
-                                <input type="email" class="form-control" id="inputEmail4">
+                                <label for="email" class="form-label">Email</label>
+                                <input type="email" class="form-control" id="email" name="email">
                             </div>
                             <div class="col-6">
-                                <label for="inputPassword4" class="form-label">Password</label>
-                                <input type="password" class="form-control" id="inputPassword4">
+                                <label for="user_id" class="form-label">User Id<spans class="text-danger">*</spans></label>
+                                <input type="text" class="form-control" id="user_id" name="user_id">
                             </div>
                             <div class="col-6">
-                                <label for="inputAddress" class="form-label">Address</label>
-                                <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+                                <label for="password" class="form-label">Password<spans class="text-danger">*</spans></label>
+                                <input type="password" class="form-control" id="password" placeholder="*********">
+                            </div>
+                            <div class="col-6">
+                                <label for="invantory_endpoint" class="form-label">Inventory Endpoint<spans class="text-danger">*</spans></label>
+                                <input type="url" class="form-control" name="inventory_endpoint" id="inventory_endpoint" placeholder="">
+                            </div>
+                            <div class="col-6">
+                                <label for="po_endpoint" class="form-label">Po Endpoint<spans class="text-danger">*</spans></label>
+                                <input type="url" class="form-control" name="po_endpoint" id="po_endpoint" placeholder="">
+                            </div>
+                            <div class="col-6">
+                                <label for="order_shipment_endpoint" class="form-label">Order Shipment Endpoint<spans class="text-danger">*</spans></label>
+                                <input type="url" class="form-control" id="order_shipment_endpoint" placeholder="">
+                            </div>
+                            <div class="col-6">
+                                <label for="order_status_endpoint" class="form-label">Order Status Endpoint<spans class="text-danger">*</spans></label>
+                                <input type="url" class="form-control" id="order_status_endpoint" placeholder="">
                             </div>
                             <div class="text-center">
                                 <button type="submit" class="btn btn-primary">Submit</button>
